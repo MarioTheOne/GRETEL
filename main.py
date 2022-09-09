25d7e00697af3260bba77b9683ec9663300254f7
MarioTheOne <m.a.prado89@outlook.com>
2022-09-09 15:40:56 +0200

    Added html documentation

--- main.py
+++ main.py
@@ -1,44 +1,40 @@
 from src.evaluation.evaluator_manager import EvaluatorManager
 import sys
 
+if __name__ == '__main__':
+    # config_file_path = '/NFSHOME/mprado/CODE/Themis/config/linux-server/set-1/config_autism_custom-oracle_dce.json'
+    # config_file_path = 'C:/Work/GNN/Mine/GRETEL2/GRETEL/config/linux-server/set-1/config_autism_custom-oracle_dce.json'
 
-config_file_path = '/NFSHOME/mprado/CODE/Themis/config/linux-server/set-1/config_autism_custom-oracle_dce.json'
+    # print('Creating the evaluation manager.......................................................')
+    # eval_manager = EvaluatorManager(config_file_path, run_number=0)
 
-print('Creating the evaluation manager.......................................................')
-eval_manager = EvaluatorManager(config_file_path, run_number=0)
+    # print('Creating the evaluators...................................................................')
+    # eval_manager.create_evaluators()
 
-print('Creating the evaluators...................................................................')
-eval_manager.create_evaluators()
-
-print('Evaluating the explainers..................................................................')
-eval_manager.evaluate()
-
-
-
+    # print('Evaluating the explainers..................................................................')
+    # eval_manager.evaluate()
 
 
 
+    #config_file_path = 'C:\\Work\\GNN\\Mine\\Themis\\config\\windows-local\\manager_config_lite.json'
+    # config_file_path = '/NFSHOME/mprado/CODE/Themis/config/linux-server/manager_caliban_lite.json'
 
+    config_file_path = sys.argv[1]
+    runno= int(sys.argv[2])
 
-#config_file_path = 'C:\\Work\\GNN\\Mine\\Themis\\config\\windows-local\\manager_config_lite.json'
-# config_file_path = '/NFSHOME/mprado/CODE/Themis/config/linux-server/manager_caliban_lite.json'
+    print('Executing:'+sys.argv[1])
 
-config_file_path = sys.argv[1]
-runno= int(sys.argv[2])
+    print('Creating the evaluation manager.......................................................')
+    eval_manager = EvaluatorManager(config_file_path, run_number=runno)
 
-print('Executing:'+sys.argv[1])
+    # print('Generating Synthetic Datasets...........................................................')
+    # eval_manager.generate_synthetic_datasets()
 
-print('Creating the evaluation manager.......................................................')
-eval_manager = EvaluatorManager(config_file_path, run_number=runno)
+    # print('Training the oracles......................................................................')
+    # eval_manager.train_oracles()
 
-# print('Generating Synthetic Datasets...........................................................')
-# eval_manager.generate_synthetic_datasets()
+    print('Creating the evaluators...................................................................')
+    eval_manager.create_evaluators()
 
-# print('Training the oracles......................................................................')
-# eval_manager.train_oracles()
-
-print('Creating the evaluators...................................................................')
-eval_manager.create_evaluators()
-
-print('Evaluating the explainers..................................................................')
-eval_manager.evaluate()
+    print('Evaluating the explainers..................................................................')
+    eval_manager.evaluate()
