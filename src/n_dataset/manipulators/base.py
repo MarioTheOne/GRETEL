be2257bbd25a745100d4d75b60c450641ffa66b2
Bardh Prenkaj <bardhprenkaj95@gmail.com>
2023-09-04 17:41:37 +0200

    begin of manipulator classes

---
+++ src/n_dataset/manipulators/base.py
@@ -0,0 +1,6 @@
+from src.core.grtl_base import Base
+
+class BaseManipulator(Base):
+        
+    def process(self, dataset):
+        return dataset
---
+++ src/n_dataset/manipulators/centralities.py
@@ -0,0 +1,9 @@
+from src.n_dataset.manipulators.base import BaseManipulator
+
+class NodeCentrality(BaseManipulator):
+    
+    
+    def process(self, dataset):
+        #todo implement
+        raise NotImplementedError()
+    
