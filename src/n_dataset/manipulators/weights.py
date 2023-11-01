324afcae8319622bc9abd97a1520c83727846e80
Hiram Borbolla Hdez <45494701+hiram91@users.noreply.github.com>
2023-11-01 16:21:32 +0100

    edge weights

--- src/n_dataset/manipulators/weights.py
+++ src/n_dataset/manipulators/weights.py
@@ -12,6 +12,7 @@ class EdgeWeights(BaseManipulator):
         # the weights need to be an array of real numbers with
         # length equal to the number of edges
         row_indices, col_indices = np.where(adj != 0)
-        weights = adj[row_indices, col_indices]
+        instance.weights = adj[row_indices, col_indices]
         
-        return { "edge_weights": list(weights) }
+        # return { "edge_weights": list(weights) }
+        return { }
