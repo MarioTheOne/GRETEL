2417d2b7380d85502b69347f5f1576a6c7806370
giovanni <g.stilo@gmail.com>
2023-11-01 14:04:22 +0100

    Heavy CLEANING

--- src/n_dataset/generators/bbbp.py
+++ src/n_dataset/generators/bbbp.py
@@ -42,7 +42,7 @@ class BBBP(Generator):
                                                             data=A, 
                                                             node_features=X, 
                                                             #edge_features=W, #TODO: GCN Message passing not take into account edges features but only weights. Look how to enable that in PyG
-                                                            graph_features={"smile":smi,"string_repp":smi}, 
+                                                            graph_features={"smile":smi,"string_repp":smi,"mol":mol}, 
                                                             dataset=self.dataset))
             else:
                 skipped_molecules += 1                
