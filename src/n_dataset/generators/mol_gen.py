9ee8afe53f22f39a9e1cbdfa9f4af59eb9ce8a5e
giovanni <g.stilo@gmail.com>
2023-11-01 16:52:22 +0100

    Integrated Hiram refactor

--- src/n_dataset/generators/mol_gen.py
+++ src/n_dataset/generators/mol_gen.py
@@ -54,7 +54,7 @@ class MolGenerator(Generator):
                                                             data=A, 
                                                             node_features=X, 
                                                             edge_features=W, #TODO: GCN Message passing not take into account edges features but only weights. Look how to enable that in PyG
-                                                            graph_features={"smile":smi,"string_repp":smi}, 
+                                                            graph_features={"smile":smi,"string_repp":smi,"mol":mol}, 
                                                             dataset=self.dataset))
             else:
                 skipped_molecules += 1   
