fd57539529f384947acb71a4b4c8f7141c1d0dd7
rmarticedeno <rmarticedeno@gmail.com>
2023-11-13 11:21:31 -0500

    rewrite forward

--- src/explainer/generative/cf2.py
+++ src/explainer/generative/cf2.py
@@ -6,11 +6,13 @@ import numpy as np
 import torch
 from dgl import from_networkx, to_networkx
 from torch.utils.data import Dataset
+from copy import deepcopy
 # import wandb
 
 from src.dataset.data_instance_base import DataInstance
 from src.dataset.data_instance_features import DataInstanceWFeaturesAndWeights
-from src.dataset.dataset_base import Dataset
+from src.n_dataset.dataset_base import Dataset
+from src.n_dataset.instances.graph import GraphInstance
 from src.core.explainer_base import Explainer
 from src.core.trainable_base import Trainable
 from src.core.oracle_base import Oracle
@@ -125,15 +127,7 @@ class ExplainModelGraph(torch.nn.Module):
         self.mask = self.build_adj_mask()
 
     def forward(self, graph, oracle):        
-        adjacency = nx.to_numpy_array(to_networkx(graph))
-        weights = graph.edata['weights'].detach().numpy()
-        features = graph.ndata['feat'].detach().numpy()
-
-        instance = DataInstanceWFeaturesAndWeights(-1)
-        instance.from_numpy_array(adjacency)
-        instance.weights = weights
-        instance.features = features 
-        pred1 = oracle.predict(instance)
+        pred1 = oracle.predict(graph)
 
         # re-build weighted adjacency matrix
         weighted_adj = self._rebuild_weighted_adj(graph)
@@ -145,10 +139,8 @@ class ExplainModelGraph(torch.nn.Module):
         # get only the edges that exist
         row_indices, col_indices = torch.where(new_weights != 0)
 
-        cf_instance = DataInstanceWFeaturesAndWeights(instance.id)
-        cf_instance.from_numpy_array(adjacency)
-        cf_instance.features = features
-        cf_instance.weights = new_weights[row_indices, col_indices].detach().numpy()
+        cf_instance = deepcopy(graph)
+        cf_instance.edge_weights = new_weights[row_indices, col_indices].detach().numpy()
         pred2 = oracle.predict(cf_instance)
 
         pred1 = torch.Tensor([pred1]).float()  # factual
