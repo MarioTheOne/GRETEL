4cde3433385f5f97dab99b32e73b7f5fd337ef7f
Mario Prado <m.a.prado89@outlook.com>
2023-12-14 14:34:11 +0100

    ported perturbation_rand as p_rand

--- src/explainer/search/dces.py
+++ src/explainer/search/dces.py
@@ -5,15 +5,28 @@ from src.core.explainer_base import Explainer
 from src.evaluation.evaluation_metric_ged import GraphEditDistanceMetric
 import numpy as np
 
+from src.core.factory_base import get_instance_kvargs
+from src.utils.cfg_utils import get_dflts_to_of, init_dflts_to_of, inject_dataset, inject_oracle, retake_oracle, retake_dataset
 
 class DCESExplainer(Explainer):
     """The Distribution Compliant Explanation Search Explainer performs a search of 
     the minimum counterfactual instance in the original dataset instead of generating
     a new instance"""
 
+    def check_configuration(self):
+        super().check_configuration()
+
+        dst_metric='src.evaluation.evaluation_metric_ged.GraphEditDistanceMetric'  
+
+        #Check if the distance metric exist or build with its defaults:
+        init_dflts_to_of(self.local_config, 'distance_metric', dst_metric)
+
+
     def init(self):
         super().init()
-        self._gd = GraphEditDistanceMetric()
+
+        self.distance_metric = get_instance_kvargs(self.local_config['parameters']['distance_metric']['class'], 
+                                                    self.local_config['parameters']['distance_metric']['parameters'])
         self.fold_id=-1
 
 
@@ -29,7 +42,7 @@ class DCESExplainer(Explainer):
             candidate_label = self.oracle.predict(ctf_candidate)
 
             if input_label != candidate_label:
-                ctf_distance = self._gd.evaluate(instance, ctf_candidate, self.oracle)
+                ctf_distance = self.distance_metric.evaluate(instance, ctf_candidate, self.oracle)
                 
                 if ctf_distance < min_ctf_dist:
                     min_ctf_dist = ctf_distance
