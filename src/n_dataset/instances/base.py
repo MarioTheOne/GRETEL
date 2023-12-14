a7c7cde03c13737b0398f26f1f14df29edc9fad4
rmarticedeno <rmarticedeno@gmail.com>
2023-12-14 05:48:00 -0500

    remove DataInstance dataset reference

--- src/n_dataset/instances/base.py
+++ src/n_dataset/instances/base.py
@@ -1,19 +1,9 @@
 class DataInstance:
     
-    def __init__(self, id, label, data, dataset=None):
+    def __init__(self, id, label, data):
         self.id = id
         self.data = data
         self.label = label #TODO: Refactoring to have a one-hot encoding of labels!
-        # empty dataset by default
-        self._dataset = dataset
-    
-    @property
-    def dataset(self):
-        return self._dataset
-    
-    @dataset.setter
-    def dataset(self, new_dataset):
-        self._dataset = new_dataset
         
     @property
     def num_nodes(self):
